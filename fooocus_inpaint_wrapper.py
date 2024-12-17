5c9f28d2421e40af08cc5b3c1bd1c32b50016db5
iacoposk8 <iacopo.guarneri@gmail.com>
2024-12-17 12:44:24 +0100

    Add files via upload

--- fooocus_inpaint_wrapper.py
+++ fooocus_inpaint_wrapper.py
@@ -3,57 +3,77 @@ import os.path
 import sys
 import json
 import requests
+from tqdm import tqdm
 from huggingface_hub import snapshot_download
 
 class FooocusInpaintWrapper:
 	def __init__(self):
-		self.node_dir = os.path.dirname(os.path.realpath(__file__))
-		self.comfyui_dir = self.node_dir.split("custom_nodes")[0]
-		self.fooocus_dir = self.node_dir + "\\Fooocus"
+		self.node_dir = os.path.normpath(os.path.dirname(os.path.realpath(__file__)))
+		self.comfyui_dir = os.path.normpath(self.node_dir.split("custom_nodes")[0])
+		self.fooocus_dir = os.path.normpath(self.node_dir + "/Fooocus")
 
-		if not os.path.isfile(self.fooocus_dir + "\\config.txt"):
+		if not os.path.isfile(os.path.normpath(self.fooocus_dir + "/config.txt")):
 			config = {
 				"path_checkpoints": [
-					self.comfyui_dir + "models\\checkpoints"
+					os.path.normpath(self.comfyui_dir + "/models/checkpoints")
 				],
 				"path_loras": [
-					self.comfyui_dir + "models\\loras"
+					os.path.normpath(self.comfyui_dir + "/models/loras")
 				],
-				"path_embeddings": self.comfyui_dir + "models\\embeddings",
-				"path_vae_approx": self.comfyui_dir + "models\\vae_approx",
-				"path_vae": self.comfyui_dir + "models\\vae",
-				"path_upscale_models": self.comfyui_dir + "models\\upscale_models",
-				"path_inpaint": self.comfyui_dir + "models\\inpaint",
-				"path_controlnet": self.comfyui_dir + "models\\controlnet",
-				"path_clip_vision": self.comfyui_dir + "models\\clip_vision",
-				"path_fooocus_expansion": self.comfyui_dir + "models\\prompt_expansion\\fooocus_expansion",
-				"path_wildcards": self.comfyui_dir + "\\wildcards",
-				"path_safety_checker": self.comfyui_dir + "models\\safety_checker",
-				"path_sam": self.comfyui_dir + "models\\sam",
-				"path_outputs": self.fooocus_dir + "\\outputs"
+				"path_embeddings": os.path.normpath(self.comfyui_dir + "/models/embeddings"),
+				"path_vae_approx": os.path.normpath(self.comfyui_dir + "/models/vae_approx"),
+				"path_vae": os.path.normpath(self.comfyui_dir + "/models/vae"),
+				"path_upscale_/models": os.path.normpath(self.comfyui_dir + "/models/upscale_/models"),
+				"path_inpaint": os.path.normpath(self.comfyui_dir + "/models/inpaint"),
+				"path_controlnet": os.path.normpath(self.comfyui_dir + "/models/controlnet"),
+				"path_clip_vision": os.path.normpath(self.comfyui_dir + "/models/clip_vision"),
+				"path_fooocus_expansion": os.path.normpath(self.comfyui_dir + "/models/prompt_expansion/fooocus_expansion"),
+				"path_wildcards": os.path.normpath(self.comfyui_dir + "/wildcards"),
+				"path_safety_checker": os.path.normpath(self.comfyui_dir + "/models/safety_checker"),
+				"path_sam": os.path.normpath(self.comfyui_dir + "/models/sam"),
+				"path_outputs": os.path.normpath(self.fooocus_dir + "/outputs")
 			}
 
-			with open(self.fooocus_dir + "\\config.txt", "w") as f:
+			with open(os.path.normpath(self.fooocus_dir + "/config.txt"), "w") as f:
 				json.dump(config, f, indent=4)
 
-		self.find_replace(self.fooocus_dir + "\\modules\\config.py", './config.txt', self.fooocus_dir + "\\config.txt")
-		self.find_replace(self.fooocus_dir + "\\modules\\config.py", './presets/default.json', self.fooocus_dir + "\\presets\\default.json")
-		self.find_replace(self.fooocus_dir + "\\args_manager.py", 'args_parser.args = args_parser.parser.parse_args()', 'args_parser.args, unknown = args_parser.parser.parse_known_args()')
+		self.find_replace(os.path.normpath(self.fooocus_dir + "/modules/config.py"), './config.txt', os.path.normpath(self.fooocus_dir + "/config.txt"))
+		self.find_replace(os.path.normpath(self.fooocus_dir + "/modules/config.py"), './presets/default.json', os.path.normpath(self.fooocus_dir + "/presets/default.json"))
+		self.find_replace(os.path.normpath(self.fooocus_dir + "/args_manager.py"), 'args_parser.args = args_parser.parser.parse_args()', 'args_parser.args, unknown = args_parser.parser.parse_known_args()')
 
-		snapshot_download(repo_id="LykosAI/GPT-Prompt-Expansion-Fooocus-v2", local_dir = self.comfyui_dir + '\\models\\prompt_expansion\\fooocus_expansion')
+		snapshot_download(repo_id="LykosAI/GPT-Prompt-Expansion-Fooocus-v2", local_dir = os.path.normpath(self.comfyui_dir + '/models/prompt_expansion/fooocus_expansion'))
 
-		destination = self.comfyui_dir + 'models\\vae_approx\\xlvaeapp.pth'
-		if not os.path.isfile(destination):
-			response = requests.get('https://huggingface.co/lllyasviel/misc/resolve/main/xlvaeapp.pth?download=true')
-			if response.status_code == 200:
-				with open(destination, 'wb') as file:
-					file.write(response.content)
+		self.file_download('https://huggingface.co/lllyasviel/misc/resolve/main/xlvaeapp.pth?download=true', os.path.normpath(self.comfyui_dir + '/models/vae_approx/xlvaeapp.pth'))
+		self.file_download('https://huggingface.co/lllyasviel/fav_models/resolve/main/fav/juggernautXL_v8Rundiffusion.safetensors?download=true', os.path.normpath(self.comfyui_dir + '/models/checkpoints/juggernautXL_v8Rundiffusion.safetensors'))
+		self.file_download('https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_offset_example-lora_1.0.safetensors?download=true', os.path.normpath(self.comfyui_dir + '/models/loras/sd_xl_offset_example-lora_1.0.safetensors'))
+
+	def file_download(self, url, save_path):
+		if os.path.isfile(save_path):
+			return True
+
+		response = requests.get(url, stream=True)
+
+		if response.status_code == 200:
+		    total_size = int(response.headers.get('content-length', 0))
+		    
+		    # Usa tqdm per mostrare la barra di avanzamento
+		    with open(save_path, 'wb') as file, tqdm(
+		        desc=save_path.split("models")[-1],
+		        total=total_size,
+		        unit='B',
+		        unit_scale=True,
+		    ) as bar:
+		        for chunk in response.iter_content(chunk_size=1024):
+		            file.write(chunk)
+		            bar.update(len(chunk))
+		else:
+		    print(f"Error: {response.status_code}")
 
 	def find_replace(self, file_path, find_text, replace_text):
 		with open(file_path, 'r') as file:
 			content = file.read()
 
-		new_content = content.replace(find_text, replace_text.replace('\\', '\\\\'))
+		new_content = content.replace(find_text, replace_text)
 
 		with open(file_path, 'w') as file:
 			file.write(new_content)
@@ -61,7 +81,7 @@ class FooocusInpaintWrapper:
 	@classmethod
 	def INPUT_TYPES(s):
 		def get_files(folder):
-			checkpoint_dir = os.path.dirname(os.path.realpath(__file__)).split("custom_nodes")[0] + "models\\" + folder
+			checkpoint_dir = os.path.normpath(os.path.dirname(os.path.realpath(__file__)).split("custom_nodes")[0] + "/models/" + folder)
 			if os.path.exists(checkpoint_dir):
 				files = [
 					f for f in os.listdir(checkpoint_dir)
@@ -74,9 +94,6 @@ class FooocusInpaintWrapper:
 		checkpoints = get_files("checkpoints")
 		loras = get_files("loras")
 
-		elem = "sd_xl_offset_example-lora_1.0.safetensors"
-		if elem not in loras:
-			loras.append(elem)
 		loras.append("None")
 
 		return {
